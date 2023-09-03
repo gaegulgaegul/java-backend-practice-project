82ef08bb97dd03922ecb264c334d56acbfc7d443
gaegulgaegul <dla9721@gmail.com>
2023-09-03 23:32:55 +0900

    refactor WebSocketBrokerConfiguration 클래스명 변경

--- web-socket-stomp-chatting/src/main/java/io/gaegul/websocketstompchatting/config/WebSocketBrokerConfiguration.java
+++ web-socket-stomp-chatting/src/main/java/io/gaegul/websocketstompchatting/config/WebSocketBrokerConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
  */
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+public class WebSocketBrokerConfiguration implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
