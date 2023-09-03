0494846d9eaeb20128e81bcd8d6e7ee390ddd5a9
gaegulgaegul <dla9721@gmail.com>
2023-09-03 15:18:04 +0900

    feat 채팅방 페이지 전달

--- web-socket-stomp-chatting/src/main/java/io/gaegul/websocketstompchatting/adapter/in/web/ChatRoomPageAdapter.java
+++ web-socket-stomp-chatting/src/main/java/io/gaegul/websocketstompchatting/adapter/in/web/ChatRoomPageAdapter.java
@@ -1,8 +1,11 @@
 package io.gaegul.websocketstompchatting.adapter.in.web;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import io.gaegul.websocketstompchatting.application.port.in.GetChatRoomUseCase;
 import lombok.RequiredArgsConstructor;
@@ -14,15 +17,29 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 class ChatRoomPageAdapter {
 	private final GetChatRoomUseCase getChatRoomUseCase;
+	private final AtomicInteger seq = new AtomicInteger(0);
 
 	/**
 	 * 채팅방 목록 페이지 전달
 	 * @param model
 	 * @return
 	 */
-	@GetMapping("/rooms")
+	@GetMapping("/chat/rooms")
 	String rooms(Model model) {
 		model.addAttribute("rooms", getChatRoomUseCase.getChatRooms());
 		return "/chat/room-list";
+	}
+
+	/**
+	 * 채팅장 페이지 전달
+	 * @param roomId
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/chat/rooms/{id}")
+	String room(@PathVariable("id") String roomId, Model model) {
+		model.addAttribute("room", getChatRoomUseCase.getChatRoom(roomId));
+		model.addAttribute("member", "member" + seq.incrementAndGet());
+		return "/chat/room";
 	}
 }
